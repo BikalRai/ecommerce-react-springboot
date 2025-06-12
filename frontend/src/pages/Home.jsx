504c1580b244c45ee9415b4a8cd18795d63ce6d6
Bikal Rai <bikalrai2785@gmail.com>
2025-06-12 16:43:29 +0545

    add cart slice and register cart reducer in store

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import { useSelector } from "react-redux";
 import LogoutButton from "../features/logout/LogoutButton";
+import Cart from "./cart/Cart";
 
 export default function Home() {
   const user = useSelector((state) => state.auth.user);
   console.log(user);
   return (
     <>
-      <h1>HOME</h1>
-      {user && <LogoutButton />}
+      {/* <h1>HOME</h1> */}
+      {/* {user && <LogoutButton />} */}
       {/* <ProductList />
       <Filter />
       <Pagination /> */}
+      <Cart />
     </>
   );
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/users/authSlice";
 import filterReducer from "./features/filter/filterSlice";
+import cartReducer from "./features/cart/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     filter: filterReducer,
+    cart: cartReducer,
   },
 });
 
